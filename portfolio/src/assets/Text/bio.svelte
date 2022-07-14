92d1b62314b0de48704c3517ddbec576f30cbae8
Brandon Wingard <wingardbrandonm@gmail.com>
2022-07-14 16:28:57 -0400

    NPM Initialization & Nav Bar Creation

---
+++ portfolio/src/assets/Text/bio.svelte
@@ -0,0 +1,1 @@
+<p>Insert Bio here: </p>
---
+++ portfolio/src/lib/NavBar.svelte
@@ -0,0 +1,10 @@
+<script>
+	import CustomButton from './CustomButton.svelte';
+	import ProjectsButton from './ProjectsButton.svelte';
+
+</script>
+
+<CustomButton text={"HOME"}/>
+<CustomButton text={"ABOUT"}/>
+<CustomButton text={"PROJECTS"}/>
+<CustomButton text={"CONTACT"}/>
