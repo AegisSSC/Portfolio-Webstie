8768877f229a7d8c003f26de8893c3d2ece4e652
Brandon Wingard <wingardbrandonm@gmail.com>
2022-09-09 13:10:49 -0400

    Created Footer and MPHero

--- portfolio/src/App.svelte
+++ portfolio/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
   import NavBar from './lib/NavBar.svelte';
+  import Footer from './lib/Footer.svelte';
+  import MainPageHero from './lib/MainPageHero.svelte';
 </script>
 
 
 <NavBar/>
+<!-- !TODO INSERT A HERO ONTO THE MAIN PAGE -->
+<MainPageHero/>
+<Footer/>
 
   <style>
     /* styles go here */
---
+++ portfolio/src/lib/MainPageHero.svelte
@@ -0,0 +1,15 @@
+
+
+<!-- !TODO REPLACE THE IMAGE USED IN THE BACKGROUND -->
+<div class="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/arch);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">Welcome to my Portfolio Website. We are currently in version: 0.0.1a. 
+        Please look through the About Me and Projects section of the website. 
+      </p>
+      <button class="btn btn-primary">About</button>
+    </div>
+  </div>
+</div>
